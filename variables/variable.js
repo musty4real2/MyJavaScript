7adb84634fc72fb0123ef7766ca823bb51ba0566
musty4real2 <musty4real2@gmail.com>
2024-03-09 17:46:13 +0100

    variable example

---
+++ variables/variable.js
@@ -0,0 +1,17 @@
+// Declaring different variables of different data types
+let firstName = 'Your Name or any name you wish to input' // first name of a person
+let lastName = 'anything you feel like ' // last name of a person
+let country = 'Nigeria the best country in the world or input your country' // country
+let city = 'abuja' // capital city of your choice
+let age = 100 // age in years of your choice
+let isMarried = true //boolean takes only true or false
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2 
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = 'world x code', //name of a person or a noun of choice
+    job = 'Programmer',
+    live = 'Nigeria'
