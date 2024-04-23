538086a0dba5c3323689a249ee6be6afa184be3c
musty4real2 <musty4real2@gmail.com>
2024-04-23 13:40:53 +0100

    example of primitive datatyping/ unmodifiable datatype

---
+++ datatype/datatype.js
@@ -0,0 +1,9 @@
+// primitive datatype/non modifiable 
+// it means the datatype cannot be mutated from string to number 
+// but the value assigned to the variable could be changed 
+
+let word = 'JavaScript';
+word="testing primitive"
+// console.log(typeof(word));
+
+console.log(word);
