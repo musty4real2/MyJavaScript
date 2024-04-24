4a0c69c42581fad58f17efb4b882dd13bf2ef635
musty4real2 <musty4real2@gmail.com>
2024-04-24 22:36:50 +0100

    changes

--- datatype/datatype.js
+++ datatype/datatype.js
@@ -55,25 +55,7 @@ Change all the string characters to capital letters using toUpperCase() method
 Change all the string characters to lowercase letters using toLowerCase() method
 Cut (slice) out the first word of the string using substr() or substring() method
 Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
-
-*/
-let challenge = '30 Days Of JavaScript';
-console.log(challenge);
-console.log(challenge.length);
-console.log(challenge.toUpperCase());
-console.log(challenge.toLowerCase());
-console.log(challenge.substring(challenge.indexOf(" ") + 1));
-console.log(challenge.slice(0, challenge.indexOf(' ')));
-console.log(challenge.includes('Script'));
-
-
-/*
-
-
-
-
 Check if the string contains a word Script using includes() method
-Split the string into an array using split() method
 Split the string 30 Days Of JavaScript at the space using split() method
 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
 Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
@@ -83,12 +65,45 @@ Use indexOf to determine the position of the first occurrence of a in 30 Days Of
 Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
 Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
 Use startsWith() method with the string 30 Days Of JavaScript and make the result true
 Use endsWith() method with the string 30 Days Of JavaScript and make the result true
 Use match() method to find all the a’s in 30 Days Of JavaScript
-Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
 Use repeat() method to print 30 Days Of JavaScript 2 times
 */
+let challenge = '30 Days Of JavaScript';
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substring(challenge.indexOf(" ") + 1));
+console.log(challenge.slice(0, challenge.indexOf(' ')));
+console.log(challenge.includes('Script'));
+console.log(challenge.split());
+console.log(challenge.split(' '));
+
+let socialString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(socialString.split(','));
+console.log(challenge.replace('JavaScript', 'Python'));
+console.log(challenge.charAt(15));
+console.log(challenge.charCodeAt('J'));
+console.log(challenge.indexOf('30 Days Of JavaScript'));
+console.log(challenge.lastIndexOf('a'));
+let sentence = 'You cannot end a sentence with because because because is a conjunction';
+console.log(sentence.indexOf('because'));
+console.log(sentence.lastIndexOf('because'));
+console.log(sentence.search('because'));
+
+let statementTrim = ' 30 Days Of JavaScript ';
+console.log(statementTrim.trim());
+console.log(challenge.startsWith(challenge));
+
+console.log(challenge.match(/a/g));
+
+let partOne = '30 Days of';
+let partTwo = 'JavaScript';
+console.log(partOne+' '+partTwo);
+console.log(challenge.repeat(2));
 
